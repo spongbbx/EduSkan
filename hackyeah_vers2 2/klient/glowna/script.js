c2a520be762e194f3f06a28643f328dfdb55f6a9
Spong <blendereqyt@gmail.com>
2023-09-15 09:22:17 +0200

    Sekcja o projekcie

--- hackyeah_vers2 2/klient/glowna/script.js
+++ hackyeah_vers2 2/klient/glowna/script.js
@@ -12,22 +12,3 @@ window.addEventListener("scroll", () => {
     menu.classList.remove("shadow");
   }
 });
-
-// zmiana tekstu przy opisie projekt
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname)
-{
-    for(tablink of tablinks)
-    {
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents)
-    {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
