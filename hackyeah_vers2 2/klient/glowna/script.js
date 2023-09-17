9c66a5f5f3b48620ff3aef9eab2fcc7e723ebbf0
Spong <blendereqyt@gmail.com>
2023-09-17 22:23:30 +0200

    side menu + poprawka autorzy

--- hackyeah_vers2 2/klient/glowna/script.js
+++ hackyeah_vers2 2/klient/glowna/script.js
@@ -1,6 +1,15 @@
 function toggleMenu() {
     var sidemenu = document.getElementById("sidemenu");
     sidemenu.classList.toggle("active"); // Toggle the 'active' class
+    const overlay = document.querySelector(".overlay");
+
+// Check the current display style and toggle it
+if (overlay.style.display === "block") {
+    overlay.style.display = "none";
+} else {
+    overlay.style.display = "block";
+}
+
 }
 
 const navbar = document.getElementById("menu");
